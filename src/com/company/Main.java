2e7b5a940d00b137aae9263e4a3ade0a10ce8cec
James Kenney <jkenney2@charter.net>
2020-10-20 17:43:00 -0400

    made a change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Now I'm a fork.");
+        System.out.println("I was just forked from Beta8397.");
+        System.out.println("I will submit a pull request.");
     }
 
 }
